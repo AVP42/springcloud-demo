6eb79f74f980c4833206d6f7fe11eef0c06549f8
AVP42 <997843848@qq.com>
2018-07-07 21:11:12 +0800

    aa

---
+++ config-client-yml/src/main/java/com/example/configclientyml/ConfigClientYmlApplication.java
@@ -0,0 +1,12 @@
+package com.example.configclientyml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientYmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientYmlApplication.class, args);
+	}
+}
--- config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -2,10 +2,10 @@ package com.example.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class ConfigClientApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
